f41cbb956c8f5a1eb2671968a33c7168af4af286
Joel Alcaraz <joelchristopheralcaraz@gmail.com>
2022-12-07 12:45:58 -0800

    Fix merge

--- app.js
+++ app.js
@@ -1,4 +1,13 @@
 /** Simple demo Express app. */
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 const express = require("express");
 const cors = require("cors");
 const app = express();
@@ -6,16 +15,22 @@ const AmazonAPI = require("./s3AmazonAPI");
 // useful error class to throw
 const { NotFoundError, BadRequestError } = require("./expressError");
 const { parseBody } = require("./middleware");
+app.use(cors());
+// process JSON body => req.body
 app.use(express.json());
 // process traditional form data => req.body
 app.use(express.urlencoded());
 app.get("/", function (req, res) {
     return res.json('hello');
 });
-app.post("/images", parseBody, function (req, res) {
-    console.log(req.body);
-    const image = AmazonAPI.upload(req.body);
-    return res.status(201).json({ image });
+app.post("/images", function (req, res) {
+    return __awaiter(this, void 0, void 0, function* () {
+        console.log('POST IMAGES');
+        console.log(req.body);
+        const image = yield AmazonAPI.upload(req.body.image);
+        console.log('REQ.BODY', req.body);
+        return res.status(201).json({ image });
+    });
 });
 /** 404 handler: matches unmatched routes. */
 app.use(function (req, res) {
--- app.ts
+++ app.ts
@@ -11,12 +11,9 @@ const AmazonAPI = require("./s3AmazonAPI");
 // useful error class to throw
 const { NotFoundError, BadRequestError } = require("./expressError");
 
-<<<<<<< HEAD
 const { parseBody } = require("./middleware");
 
-=======
 app.use(cors());
->>>>>>> c6a6eb0af07cb8d67aa1c15a0d3080adfdc350f9
 // process JSON body => req.body
 app.use(express.json());
 
@@ -27,17 +24,11 @@ app.get("/", function(req,res) {
   return res.json('hello')
 })
 
-<<<<<<< HEAD
-app.post("/images", parseBody, function(req, res) {
-  console.log(req.body);
-  const image = AmazonAPI.upload(req.body);
-=======
 app.post("/images", async function(req, res) {
   console.log('POST IMAGES');
   console.log(req.body);
   const image = await AmazonAPI.upload(req.body.image);
   console.log('REQ.BODY', req.body);
->>>>>>> c6a6eb0af07cb8d67aa1c15a0d3080adfdc350f9
   return res.status(201).json({ image });
 });
 
